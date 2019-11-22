ee150767497b9fda2aae45a2fbe3cdf690fbfa77
somebelly <afsomebelly@gmail.com>
2019-11-22 13:21:00 +0100

    ver 0.4.1. Keywords??

--- konachan-viewer.py
+++ konachan-viewer.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python3
 
-import os, sys, shutil, eel
+import os, sys, eel
 from base64 import b64encode
 from datetime import date, datetime, timedelta
 from random import randint, choice
 from requests_html import HTMLSession
+from screeninfo import get_monitors
+from PIL import Image, ImageOps
+from io import BytesIO
 
 web_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'web')
+
 current = []
 last_call = None
 cache_size = 3
+img_keywords = []
+
 session = HTMLSession()
 beginning = {
     "konachan.com": date(2008, 1, 13),
     "yande.re": date(2007, 8, 8),
 }
+try:
+    monitor = get_monitors()[0]
+    screen_size = (monitor.width, monitor.height)
+    screen_ratio = monitor.width / monitor.height
+except Exception as e:
+    print(e)
+    screen_size = (1920, 1080)
+    screen_ratio = 16 / 9
+window_size = (screen_size[0] / screen_ratio, screen_size[1] / screen_ratio)
 
 
 @eel.expose
 def update_last_call():
     global last_call
     last_call = datetime.now()
+
+
+@eel.expose
+def update_img_keywords(keywords):
+    global img_keywords
+    img_keywords = keywords
+    print(img_keywords)
 
 
 def check_alive(seconds):
@@ -34,41 +56,75 @@ def random_date(start=date.today() - timedelta(days=1000), end=date.today()):
     return start + timedelta(days=randint(randint(0, size), size))
 
 
-def get_random_image_url(site=None):
+# text="size_x x size_y"
+def get_size(text):
+    size = text.split('x')
+    return (int(size[0]), int(size[-1]))
+
+
+def image_filter(img_html_list, keywords=None):
+    img_list = [(img.links.pop(), get_size(img.text)) for img in img_html_list]
+    if keywords:
+        for keyword in keywords:
+            img_list = [(link, size) for link, size in img_list
+                        if keyword in link]
+    return img_list
+
+
+def get_random_image_url(site=None, keywords=None):
     site_list = ["konachan.com", "yande.re"]
     if not site or site not in site_list:
         site = choice(site_list)
     d = random_date(start=beginning[site])
     url = f"https://{site}/post/popular_by_day?day={d.day}&month={d.month}&year={d.year}"
     try:
-        html = choice(session.get(url).html.find(".directlink.largeimg"))
-        return html.links.pop()
-    except:
-        return ''
+        html_list = session.get(url).html.find(".directlink.largeimg")
+        return choice(image_filter(html_list, keywords))
+    except Exception as e:
+        print(e)
+        return ()
 
 
-def get_image(url):
+def size_in_range(size, r=0.45):
+    return (screen_ratio * (1 - r) < size[0] / size[1] < screen_ratio *
+            (1 + r))
+
+
+def get_image(url, size):
     try:
-        return b64encode(session.get(url).content)
-    except:
+        image = Image.open(BytesIO(session.get(url).content))
+        if size_in_range(size):
+            image = ImageOps.fit(image, screen_size, method=Image.LANCZOS)
+        elif size_in_range(size[::-1]):
+            image = image.rotate(choice([90, -90]))
+            image = ImageOps.fit(image, screen_size, method=Image.LANCZOS)
+        buffered = BytesIO()
+        image.save(buffered, format='webp')
+        return b64encode(buffered.getvalue())
+    except Exception as e:
+        print(e)
         return b''
 
 
 def update():
-    while True:
-        url = get_random_image_url()
+    start = datetime.now()
+    while (datetime.now() - start).seconds < 10:
+        url = get_random_image_url(keywords=img_keywords)
         if url:
-            img = get_image(url)
+            img = get_image(url[0], url[1])
             if img:
-                current.append((str(img)[2:-1], url))
+                current.append((str(img)[2:-1], url[0]))
                 if len(current) >= cache_size:
                     current.pop(0)
                 return
+        eel.sleep(1)
+    print('Timeout.')
+    true_close()
 
 
 def keep_loading(seconds):
     while True:
-        eel.loading([1, current[-1][0], current[-1][1]])()
+        eel.loading([1, current[-1]])()
         eel.spawn(update)
         eel.sleep(seconds)
 
@@ -101,6 +157,8 @@ def init(seconds):
         all_interfaces=True,
         block=False,
         close_callback=fake_close,
+        # size=window_size,
+        cmdline_args=['--start-fullscreen'],
     )
     eel.spawn(keep_calling, seconds / 5)
     eel.spawn(keep_loading, seconds)
--- web/main.js
+++ web/main.js
@@ -1,11 +1,16 @@
 /* global eel */
 
-// call: [0]; update: [1, img, url]
+// call: [0]; update: [1, (img, url)]
 eel.expose(loading)
 function loading (res) {
   if (res[0] === 1) {
-    document.getElementById('image').src = 'data:image;base64,' + res[1]
-    console.log(res[2])
+    const img = document.getElementById('image')
+    img.src = 'data:image;base64,' + res[1][0]
+    console.log(res[1][1])
   }
   eel.update_last_call()
+}
+
+function updateKeywords (keywords) {
+  eel.update_img_keywords(keywords)
 }
